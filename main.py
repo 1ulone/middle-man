fe935fe9b5ec29d2bd19ec1f295c4a9037615b50
ulone <42242738+1ulone@users.noreply.github.com>
2025-12-16 17:57:26 +0700

    base_64

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from fastapi import FastAPI, Request, HTTPException
+import os
+import base64
+import tempfile
+from fastapi import FastAPI, Request
 import firebase_admin
 from firebase_admin import credentials, db
-import os
 
-cred = credentials.Certificate("serviceAccount.json")
+# Decode base64 service account and write to a temporary file
+sa_json_base64 = os.environ["FIREBASE_SA_BASE64"]  # your env var
+sa_json_bytes = base64.b64decode(sa_json_base64)
+
+with tempfile.NamedTemporaryFile(delete=False) as f:
+    f.write(sa_json_bytes)
+    tmp_path = f.name
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate(tmp_path)
 firebase_admin.initialize_app(cred, {
     "databaseURL": os.environ["FIREBASE_DB_URL"]
 })
