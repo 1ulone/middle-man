4d505f646775a6e0a0bd8747cbe4c25631becacf
ulone <42242738+1ulone@users.noreply.github.com>
2025-12-16 18:11:01 +0700

    testing

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-import os
-import base64
-import tempfile
+# import os
+# import base64
+# import tempfile
 from fastapi import FastAPI, Request
-import firebase_admin
-from firebase_admin import credentials, db
 
-# Decode base64 service account and write to a temporary file
-sa_json_base64 = os.environ["FIREBASE_SA_BASE64"]  # your env var
-sa_json_bytes = base64.b64decode(sa_json_base64)
-
-with tempfile.NamedTemporaryFile(delete=False) as f:
-    f.write(sa_json_bytes)
-    tmp_path = f.name
-
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate(tmp_path)
-firebase_admin.initialize_app(cred, {
-    "databaseURL": os.environ["FIREBASE_DB_URL"]
-})
+# import firebase_admin
+# from firebase_admin import credentials, db
+#
+# # Decode base64 service account and write to a temporary file
+# sa_json_base64 = os.environ["FIREBASE_SA_BASE64"]  # your env var
+# sa_json_bytes = base64.b64decode(sa_json_base64)
+#
+# with tempfile.NamedTemporaryFile(delete=False) as f:
+#     f.write(sa_json_bytes)
+#     tmp_path = f.name
+#
+# # Initialize Firebase Admin SDK
+# cred = credentials.Certificate(tmp_path)
+# firebase_admin.initialize_app(cred, {
+#     "databaseURL": os.environ["FIREBASE_DB_URL"]
+# })
 
 app = FastAPI()
 
@@ -25,7 +26,7 @@ app = FastAPI()
 async def upload(req: Request):
     data = await req.json()
 
-    ref = db.reference("d01")
-    ref.push(data)
+    #ref = db.reference("d01")
+    #ref.push(data)
 
     return {"ok": True}
